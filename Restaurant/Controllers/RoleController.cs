750881fd61496578a2700c2c687c4fe3e7d084cf
adhee <adheenaus@gmail.com>
2019-05-04 02:20:24 -0700

    added back roles

---
+++ Restaurant/Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Data;
+using Restaurant.Repositories;
+
+namespace Restaurant.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext _context;
+        public RoleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            RoleRepo roleRepo = new RoleRepo(_context);
+            return View();
+        }
+    }
+}
