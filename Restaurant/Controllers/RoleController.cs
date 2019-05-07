71e8ad84a2894c7ba56a5309ee3584d8a19ad4f4
adhee <adheenaus@gmail.com>
2019-05-06 23:11:08 -0700

    added roles

--- Restaurant/Controllers/RoleController.cs
+++ Restaurant/Controllers/RoleController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.Data;
 using Restaurant.Repositories;
+using Restaurant.ViewModel;
 
 namespace Restaurant.Controllers
 {
@@ -21,5 +22,28 @@ namespace Restaurant.Controllers
            // IEnumerable < RoleRepo > = roleRepo.GetAllRoles();
             return View(roleRepo.GetAllRoles());
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RoleVM roleVM)
+        {
+            if (ModelState.IsValid)
+            {
+                RoleRepo roleRepo = new RoleRepo(_context);
+                var success = roleRepo.CreateRole(roleVM.RoleName);
+                if (success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            ViewBag.Error = "An error occurred while creating this role. Please try again.";
+            return View();
+        }
+
     }
 }
