b379420b61cc027dd635aa8158a201980cf1517d
adhee <adheenaus@gmail.com>
2019-05-07 15:22:22 -0700

    added View

--- Restaurant/Controllers/FoodItemController.cs
+++ Restaurant/Controllers/FoodItemController.cs
@@ -31,7 +31,7 @@ namespace Restaurant.Controllers
         [HttpPost]
         public IActionResult Create(FoodItem food)
         {
-                     
+            return View();       
         }
     }
 }
